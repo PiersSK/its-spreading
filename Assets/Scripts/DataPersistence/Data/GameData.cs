351c7b712cf2da5f3f900dbe356f5b3bd61b68fe
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2024-01-16 20:14:06 +0000

    Adds manager for saving & loading data.
    Temporarily added in spreadEventsTriggered and daysComplete as things we want to track, these can be expanded upon once we've deciced what data we want to save.

---
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData : MonoBehaviour
+{
+    public int spreadEventsTriggered;
+    public int daysComplete;
+
+    // default values for when the game starts with no data.
+    public GameData()
+    {
+        this.spreadEventsTriggered = 0;
+        this.daysComplete = 0;
+    }
+}
