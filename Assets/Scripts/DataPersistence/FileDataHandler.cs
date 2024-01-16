1981473c5015245cc7eb896b607de6c27b6b53ae
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2024-01-16 21:55:15 +0000

    Adds save to file functionality
    Moved some files, no biggie.
    
    Now tracks the amount of confetti events have been triggered
    
    Fully adds in functionality for saving and loading from a file.

---
+++ Assets/Scripts/DataPersistence/FileDataHandler.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class FileDataHandler
+{
+    private string dataDirPath = "";
+
+    private string dataFileName = "";
+
+    public FileDataHandler(string dataDirPath, string dataFileName)
+    {
+        this.dataDirPath = dataDirPath;
+        this.dataFileName = dataFileName;
+    }
+
+    public GameData Load()
+    {
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        GameData loadedData = null;
+        if (File.Exists(fullPath))
+        {
+            try
+            {
+                //Load data from file
+                string dataToLoad = "";
+                using (FileStream stream = new FileStream(fullPath, FileMode.Open))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        dataToLoad = reader.ReadToEnd();
+                    }
+                }
+                
+                //Make like Heavy Rain and de-jsonify
+                loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error occurred when trying to load dat afrom file: " + fullPath + "\n" + e);
+            }
+
+
+
+        }
+        return loadedData;
+    }
+
+    public void Save(GameData data)
+    {
+        string fullPath = Path.Combine(dataDirPath, dataFileName);
+        try
+        {
+            //Creates a directory if one doesn't already exist
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+            //serialize game data into json
+            string dataToStore = JsonUtility.ToJson(data, true);
+
+            //write file to file system
+            using (FileStream stream = new FileStream(fullPath, FileMode.Create))
+            {
+                using(StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(dataToStore);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error occurred when trying to save data to file: " + fullPath + "\n" + e);
+        }
+    }
+}
