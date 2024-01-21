6e9562dd0d40bd2678120f452be449afd6cf86d5
OrcaOfTime <mwilliams.sean@gmail.com>
2024-01-21 13:14:58 +0000

    Bread event finalised
    Fixed bread positioning. Bread spreading only triggers before 10 am

--- Assets/Scripts/SpreadingEvents/BreadSpreadingEvent.cs
+++ Assets/Scripts/SpreadingEvents/BreadSpreadingEvent.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.WSA;
 
@@ -7,22 +9,14 @@ public class BreadSpreadingEvent : SpreadingEvent
 {
     [SerializeField] private Bread bread;
 
-    [SerializeField] private float breadSpreadTimeRequired = 10f;
-    [SerializeField] private float breadSpreadTime = 0f;
-
     protected override void Update()
     {
         base.Update();
-
-        if (bread.isSpreading && !eventComplete)
-        {
-            breadSpreadTime += Time.deltaTime;
-        }
     }
 
     protected override bool ShouldEventTrigger()
     {
-        return breadSpreadTime <= breadSpreadTimeRequired && bread.isSpreading;
+        return !TimeController.Instance.TimeHasPassed(10, 0) && bread.isSpreading;
     }
 
     protected override void EventImpact()
