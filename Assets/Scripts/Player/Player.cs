1cc437e56c50d44dba4568e7413a78bae4ef9b98
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2024-01-21 11:39:04 +0000

    removes unused import effecting builds

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.InputSystem;
 public class Player : MonoBehaviour, IDataPersistence
 {
     public static Player Instance { get; private set; }
