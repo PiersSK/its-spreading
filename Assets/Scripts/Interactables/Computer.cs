e82af9baea8eb627b2ac1beb51539fb15ba4a387
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2024-01-21 14:49:22 +0000

    adds comp interact

---
+++ Assets/Scripts/Interactables/Computer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+    }
+}
