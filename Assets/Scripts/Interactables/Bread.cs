1e06e5cbd17c58f1a8f1669a12d43ab53cb98260
OrcaOfTime <mwilliams.sean@gmail.com>
2024-01-22 12:45:22 +0000

    Actual audio clip set up
    Got my shit together and implemented audio properly

--- Assets/Scripts/Interactables/Bread.cs
+++ Assets/Scripts/Interactables/Bread.cs
@@ -8,24 +8,22 @@ public class Bread : Interactable
 
     private const string JAM = "Jam";
     private AudioSource _audioSource;
-    [SerializeField] AudioClip _audioClip;
 
     // Start is called before the first frame update
     void Start()
     {
         promptText = JAM;
         _audioSource = GetComponent<AudioSource>();
-        _audioClip = GetComponent<AudioClip>();
     }
 
     // Update is called once per frame
     public override void Interact()
     {
-        if(!isSpreading)
+        if (!isSpreading)
         {
             isSpreading = true;
             transform.Rotate(0, 180, 0, Space.Self);
-            _audioSource.PlayOneShot(_audioClip);
+            _audioSource.Play();
         }
     }
 
