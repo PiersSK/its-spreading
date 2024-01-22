9949633ad2f9971e5f5d9340654c21ed38f59268
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2024-01-22 12:55:59 +0000

    Please let this work.

---
+++ Assets/Scripts/UI/PhoneUI/PhoneUI.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneUI : MonoBehaviour
+{
+    public static PhoneUI Instance { get; private set; }
+    public Image phoneBackground;
+    public List<GameObject> notifications;
+    private bool isPhoneDisplayed = false;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
