78ea67151a997eb890c5a2859ab62e498f2f4cb6
JamesYJ95 <41065380+JamesYJ95@users.noreply.github.com>
2024-01-23 12:17:15 +0000

    Saves current score

--- Assets/Scripts/DataPersistence/Data/GameData.cs
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -9,8 +9,8 @@ public class GameData
     public int daysComplete;
     public float currentTime;
     public Vector3 playerPosition;
-
     public List<string> completeEvents;
+    public int numCompleteEvents;
 
     // default values for when the game starts with no data.
     public GameData()
@@ -20,5 +20,6 @@ public class GameData
         this.playerPosition = new Vector3 (0, 1.1f, 0);
         this.currentTime = 8;
         this.completeEvents = new List<string>();
+        this.numCompleteEvents = 0;
     }
 }
--- Assets/Scripts/GameSystems/ObjectiveController.cs
+++ Assets/Scripts/GameSystems/ObjectiveController.cs
@@ -37,9 +37,11 @@ public class ObjectiveController : MonoBehaviour, IDataPersistence
     {
         historicStatDisplay.text = HISTSTATPREFIX + data.spreadEventsTriggered;
         objectivesCompleteAllTime = data.spreadEventsTriggered;
+        ObjectivesComplete = data.numCompleteEvents;
     }
     public void SaveData(ref GameData data)
     {
         data.spreadEventsTriggered += objectivesComplete;
+        data.numCompleteEvents = ObjectivesComplete;
     }
 }
