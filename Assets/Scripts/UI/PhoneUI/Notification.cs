5f0c5199feeaa2aca930fffe454d67472ed76033
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2024-01-23 19:34:09 +0000

    adds system for notifications
    allows us to schedule new ones during the day etc....

---
+++ Assets/Scripts/UI/PhoneUI/Notification.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Notification : MonoBehaviour
+{
+    public PhoneUI.PhoneApp appsource;
+    public string messageBody;
+    public Color backgroundColor;
+
+    [SerializeField] private TextMeshProUGUI title;
+    [SerializeField] private TextMeshProUGUI body;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(NotificationClicked);
+        UpdateNotification();
+    }
+
+    public void UpdateNotification()
+    {
+        backgroundColor.a = 0.75f;
+        GetComponent<Image>().color = backgroundColor;
+        title.text = appsource.ToString();
+        body.text = messageBody;
+    }
+
+    private void NotificationClicked()
+    {
+        switch (appsource)
+        {
+            case PhoneUI.PhoneApp.Messages:
+                //PhoneUI.Instance.notificationPage.SetActive(false);
+                //PhoneUI.Instance.messagePage.SetActive(true);
+                break;
+            default:
+                break;
+        }
+
+        PhoneUI.Instance.notifications.Remove(this);
+        Destroy(gameObject);
+    }
+}
