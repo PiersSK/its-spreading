450803e7b6811c47f78628c51237b837c6e89261
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2024-01-25 12:44:33 +0000

    adds virus prompt thought

--- Assets/Scripts/SpreadingEvents/VirusSpreadingEvent.cs
+++ Assets/Scripts/SpreadingEvents/VirusSpreadingEvent.cs
@@ -10,14 +10,33 @@ public class VirusSpreadingEvent : SpreadingEvent
     [SerializeField] private int minOfBroadcastStart = 0;
     [SerializeField] private int hourOfBroadcastEnd = 18;
     [SerializeField] private int minOfBroadcastEnd = 30;
+
+    private bool hasWatched = false;
+    private bool hasReacted = false;
+
+    private const string INITIALTHOUGHT = "Oh spaghettios, they're considering a lockdown in response to the spreading of the gigglepox pandemic.";
+
     protected override void Update()
     {
         base.Update();
-
+        if(!hasWatched
+            && (TimeController.Instance.IsInTimeSpan(hourOfBroadcastStart, minOfBroadcastStart, hourOfBroadcastEnd, minOfBroadcastEnd)
+            && sofa.isLockedIn))
+        {
+            hasWatched = true;
+            ThoughtBubble.Instance.ShowThought(INITIALTHOUGHT);
+            Invoke(nameof(EventEnd), 5f);
+        }
     }
+
+    private void EventEnd()
+    {
+        hasReacted = true;
+    }
+
 
     protected override bool ShouldEventTrigger()
     {
-        return (TimeController.Instance.IsInTimeSpan(hourOfBroadcastStart, minOfBroadcastStart, hourOfBroadcastEnd, minOfBroadcastEnd) && sofa.isLockedIn);
+        return hasReacted;
     }
 }
