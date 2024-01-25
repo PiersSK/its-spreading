99557403471b4b7ef5adc3c24cc8bf454433f436
Piers Stuart-Kelso <35305076+PiersSK@users.noreply.github.com>
2024-01-25 14:17:11 +0000

    adds dance and love

--- Assets/Scripts/UI/Dialogue/ResponseClasses/SisResponse.cs
+++ Assets/Scripts/UI/Dialogue/ResponseClasses/SisResponse.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class SisResponse : DialogueResponse
 {
+    private const string FIRSTTHOUGHT = "Those tickets sounded important to her, she shouldn't miss out.";
     public void CloseWithSuccess()
     {
         LoveSpreadingEvent.Instance.calledSis = true;
         Player.Instance.TogglePlayerIsEngaged();
         CloseDialogue();
+        ThoughtBubble.Instance.ShowThought(FIRSTTHOUGHT);
     }
 
     public void CloseWithSpread()
