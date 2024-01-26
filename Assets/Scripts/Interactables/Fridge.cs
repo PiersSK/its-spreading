c2dcde0eb6e069aabe703997385e224665a6e67f
OrcaOfTime <mwilliams.sean@gmail.com>
2024-01-26 15:38:19 +0000

    Extra step and reminder
    Added need to interact with fridge after petri has gone rotten to complete event and a thought bubble reminding the player to check on their petri friend

--- Assets/Scripts/Interactables/Fridge.cs
+++ Assets/Scripts/Interactables/Fridge.cs
@@ -7,15 +7,33 @@ public class Fridge : Interactable
 {
 
     [SerializeField] public bool petriInFridge = false;
-    [SerializeField] bool petriRotten = false;
+    [SerializeField] public bool postRottenInteract = false;
+    public bool reminderSent = false;
+
+    [SerializeField] private int rottingTimeSpan = 1;
+    [SerializeField] private int checkOnFriendReminderminutes = 30;
 
     [SerializeField] public ParticleSystem stinkParticles;
+
+    [SerializeField] private string checkFriendReminder = "I wonder how my petri friend is doing?";
 
     public TimeSpan petriInFridgeTime;
 
     public void Awake()
     {
         petriInFridgeTime = new TimeSpan();
+    }
+
+    public void Update()
+    {
+        if (IsFriendRotten() && TimeController.Instance.TimeHasPassed(petriInFridgeTime.Hours + rottingTimeSpan, petriInFridgeTime.Minutes + checkOnFriendReminderminutes))
+        {
+            if (!reminderSent)
+            {
+                ThoughtBubble.Instance.ShowThought(checkFriendReminder);
+                reminderSent = true;
+            }
+        }
     }
 
     public override void Interact()
@@ -26,12 +44,16 @@ public class Fridge : Interactable
             Player.Instance.newInventory.RemoveItem("petriDish");
             petriInFridgeTime = TimeController.Instance.CurrentTime();
             petriInFridge = true;
+        } 
+        else if (IsFriendRotten())
+        {
+            postRottenInteract = true;
         }
     }
 
     public override bool CanInteract()
     {
-        return Player.Instance.newInventory.HasItem("petriDish");
+        return Player.Instance.newInventory.HasItem("petriDish") || IsFriendRotten();
     }
 
     public void SetParticleEmission()
@@ -39,5 +61,10 @@ public class Fridge : Interactable
         var emission = stinkParticles.emission;
         emission.rateOverTime = 12;
         stinkParticles.Play();
+    }
+
+    private bool IsFriendRotten()
+    {
+       return petriInFridge && TimeController.Instance.TimeHasPassed(petriInFridgeTime.Hours + rottingTimeSpan, petriInFridgeTime.Minutes);
     }
 }
--- Assets/Scripts/SpreadingEvents/MouldSpreadingEvent.cs
+++ Assets/Scripts/SpreadingEvents/MouldSpreadingEvent.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class MouldSpreadingEvent : SpreadingEvent
 {
     [SerializeField] private Fridge _fridge;
-    [SerializeField] private int rottingTimeSpan = 1;
 
     private string mouldSpreadThought;
 
     protected override bool ShouldEventTrigger()
     {
-        return _fridge.petriInFridge && TimeController.Instance.TimeHasPassed(_fridge.petriInFridgeTime.Hours + rottingTimeSpan, _fridge.petriInFridgeTime.Minutes);
+        return _fridge.petriInFridge && _fridge.postRottenInteract;
     }
 
     protected override void EventImpact()
@@ -19,6 +18,7 @@ public class MouldSpreadingEvent : SpreadingEvent
         mouldSpreadThought = Player.Instance.playerBelievesInDarren ? "Oh Magic Darren what's that smell?!" : "Oh God what's that smell?!";
         ThoughtBubble.Instance.ShowThought(mouldSpreadThought);
         _fridge.SetParticleEmission();
+        _fridge.reminderSent = true;
         base.EventImpact();
     }
 }
