3b0a387c5652ce3fbcd3a56415799515f0612d9d
Piers Stuart-Kelso <piers.stuart-kelso@hotmail.co.uk>
2024-03-07 23:48:55 +0000

    phone no longer hides on pause
    removes random unused UI code

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,11 +7,7 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance {  get; private set; }
 
-    private GameObject currentTopUI;
-
-    [SerializeField] private GameObject computerUI;
     [SerializeField] private GameObject pauseUI;
-    [SerializeField] private GameObject phoneUI;
 
     private void Awake()
     {
@@ -24,39 +20,9 @@ public class UIManager : MonoBehaviour
         ThoughtBubble.Instance.UpdateThoughtQueue();
     }
 
-    private void DeactivateAllUI()
-    {
-        computerUI.SetActive(false);
-        pauseUI.SetActive(false);
-        phoneUI.SetActive(false);
-    }
-
-    public void ToggleComputerUI()
-    {
-        ToggleUIElement(computerUI);
-    }
-
     public void TogglePauseUI()
     {
-        ToggleUIElement(pauseUI);
+        pauseUI.SetActive(!pauseUI.activeSelf);
     }
-
-    public void TogglePhoneUI()
-    {
-        ToggleUIElement(phoneUI);
-    }
-
-    private void ToggleUIElement(GameObject uiElement)
-    {
-        if (currentTopUI != uiElement) currentTopUI = uiElement;
-        else currentTopUI = null;
-
-        bool shouldBeActive = !uiElement.activeSelf;
-        DeactivateAllUI();
-        uiElement.SetActive(shouldBeActive);
-    }
-
-   
-
 
 }
